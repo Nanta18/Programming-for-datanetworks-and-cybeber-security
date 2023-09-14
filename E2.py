522137dc4055c8b40dda748d61d8751af0073339
Joonatan Vainiomäki <joonatan.vainiomaki@protonmail.com>
2023-09-14 16:57:37 +0300

    Added comments

--- E2.py
+++ E2.py
@@ -1,6 +1,7 @@
 input_strings = ["Dog", "Cat", "Moose", "Mouse", "Rhino", "Dodo", "Manatee"]
 
 def is_first_char_m(sana):
+    # Returns the words with a or b in front of them depending if the word starts with "m". amoose or brhino as an example 
     if sana[0].lower() == 'm':
         return 'a' + sana
     else:
