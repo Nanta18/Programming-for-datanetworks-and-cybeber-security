4fcdf7350d652cabf5c7c7a449318bfb54a60968
Joonatan Vainiomäki <joonatan.vainiomaki@protonmail.com>
2023-09-14 17:00:10 +0300

    Added commets

--- E1.py
+++ E1.py
@@ -1,7 +1,7 @@
-
 def count_strings (strings):
     count = 0
     for i in strings:
+        #checks if the word is longer than 2 characters and if first and last characters match. Adds +1 to count if both are true
         if len(i) >= 2 and i[0] == i[-1]:
             count+= 1
     return count 
